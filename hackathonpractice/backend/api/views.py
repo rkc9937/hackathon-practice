f140a7afa0262ec11835f7a8814c60d3decf9ea8
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2024-09-28 17:05:37 -0400

    Added dummy data to imgSearch endpoint

--- hackathonpractice/backend/api/views.py
+++ hackathonpractice/backend/api/views.py
@@ -9,26 +9,35 @@ from PIL import Image  # Make sure you have Pillow installed
 
 from .serializers import ImageUploadSerializer
 
-@api_view(['POST'])
+@api_view(['GET'])
 def healthcheck(request):
     return Response({"message": "Got some data!"})
 
 @api_view(['POST'])
 def imgSearch(request):
-    serializer = ImageUploadSerializer(data=request.data)
+    # data: {img: data}
+    img_b64 = request.data["img"]
+    print("Img submitted is: " + img_b64)
 
-    if serializer.is_valid():
-        image = serializer.validated_data['image']
+    response_data = {
+        "results": [
+            {
+                "item_name": "Cool pokemon card",
+                "item_description": "The coolest pokemon card ever",
+                "item_price": 19.89
+            },
+            {
+                "item_name": "Cool yugioh card",
+                "item_description": "The coolest yugioh card ever",
+                "item_price": 5.00
+            },
+            {
+                "item_name": "Basketball",
+                "item_description": "Used by MJ himself",
+                "item_price": 10000009.89
+            },
+        ]
+    }
 
-        # Open the image using Pillow
-        img = Image.open(image)
-
-        # Convert the image to bytes
-        buffered = BytesIO()
-        img.save(buffered, format="JPEG")  # You can change the format if needed
-        img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
-
-        return Response({'image_base64': img_str}, status=status.HTTP_200_OK)
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(response_data, status=status.HTTP_200_OK)
 
