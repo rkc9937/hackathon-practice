7c201f295a76ab81a809e53a21a72552881698d2
Michael Kim <mkmichael.kim@mail.utoronto.ca>
2024-09-28 14:37:30 -0400

    Change in the setting for rest framework

--- hackathonpractice/backend/api/views.py
+++ hackathonpractice/backend/api/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse
 # Create your views here.
 
 @api_view(['POST'])
 def healthcheck(request):
-    return HttpResponse({"message": "Got some data!"})
+    return Response({"message": "Got some data!"})
 
 @api_view(['POST'])
 def imgSearch(request):
-    return HttpResponse({"message": "Got some data!"})
+    return Response({"message": "Got some data!"})
 
