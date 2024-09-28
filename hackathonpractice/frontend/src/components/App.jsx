82b630a45276ec0154ceaa6cf9d667f0f631d35c
Rishabh <rishabh.chowdhary@mail.utoronto.ca>
2024-09-28 13:08:23 -0400

    frontend init

---
+++ hackathonpractice/frontend/src/components/App.jsx
@@ -0,0 +1,14 @@
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
+
+function App() {
+    return (
+        <div>
+            <Button variant="contained">Hello World</Button>
+        </div>
+    );
+}
+export default App;
