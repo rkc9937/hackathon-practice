69b0cd51b5b452470f2422b8f767cec4bf5ecd39
Michael Kim <mkmichael.kim@mail.utoronto.ca>
2024-09-28 14:15:47 -0400

    Healthcheck views

--- hackathonpractice/backend/api/views.py
+++ hackathonpractice/backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import HttpResponse
 # Create your views here.
+
+@api_view(['POST'])
+def healthcheck(request):
+    return HttpResponse({"message": "Got some data!"})
